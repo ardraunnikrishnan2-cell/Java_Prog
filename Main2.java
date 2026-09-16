91ed48b92194f27b8f629a71fa322ddff36fb9e1
Ardra Unnikrishnan <ardraunnikrishnan2@gmail.com>
2026-09-16 20:27:21 +0530

    Addition giving the target
    Find two elements whose addition gives the target.

---
+++ Main2.java
@@ -0,0 +1,28 @@
+public class Main2 
+{
+    public static void main(String[] args) {
+
+        int[] arr = {4, 5, 21, 30};
+        int target = 26;
+
+        boolean found = false;
+
+        for (int i = 0; i < arr.length; i++) {
+
+            for (int j = i + 1; j < arr.length; j++) {
+
+                if (arr[i] + arr[j] == target) {
+                    System.out.println("Target found using: "
+                            + arr[i] + " + " + arr[j] + " = " + target);
+
+                    found = true;
+                }
+            }
+        }
+
+        if (!found) {
+            System.out.println("No two elements add up to the target.");
+        }
+    }
+} 
+
